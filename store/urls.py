26a682622e9874203cd43e6d84eb4e7a4a27dd7a
Shivam <shiv240898@gmail.com>
2022-07-18 20:42:04 +0530

    Initial commit

---
+++ store/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.index, name="home"),
+    path('signup', views.signup , name='signup'),
+    path('login/', views.login , name='login'),
+    path('logout', views.logout, name="logout"),
+    path('store/', views.store, name='store'),
+    path('cart/', views.cart, name='cart'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('update_item/', views.updateItem, name='update_item'),
+    path('process_order/', views.processOrder, name='process_order'),
+]
